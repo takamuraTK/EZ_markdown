309e1e08061c94f1d91d3d042b20222393b57254
takamuraTK <tirpitz.zzz@outlook.jp>
2020-04-18 16:24:27 +0900

    indexとdetail画面の作成

--- article/models.py
+++ article/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Report(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.CharField(max_length=3000)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
--- article/views.py
+++ article/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Report
 
 def index(request):
-    return HttpResponse('記事一覧画面')
+    report_list = Report.objects.all()
+    context = {'report_list': report_list}
+    return render(request, 'article/index.html', context)
+
+def detail(request, report_id):
+    report = get_object_or_404(Report, pk=report_id)
+    return render(request, 'article/detail.html', {'report': report})
+
